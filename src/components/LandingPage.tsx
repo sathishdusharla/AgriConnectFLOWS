08ba14e286018bb7860f6a5f873358d223113d40
Sathish Dusharla <thedusharla1@gmail.com>
2026-06-22 22:59:07 +0530

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -125,17 +125,7 @@ export const LandingPage: React.FC<LandingPageProps> = ({ onNavigate }) => {
 
         {/* Footer */}
         <div className="mt-8 text-center">
-          <p className="text-xs text-gray-500">
-            {' '}
-            <a 
-              href="https://linkedin.com/in/sathishdusharla" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="text-green-600 hover:text-green-800 underline cursor-pointer transition-colors"
-            >
-              @Sathish Dusharla
-            </a>
-          </p>
+          
         </div>
       </div>
     </div>
